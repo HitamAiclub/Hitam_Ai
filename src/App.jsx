42e876d90c7c6f26f8196f8f52356f11b4cd80c9
MDArif <arifsmart2005@gmail.com>
2025-08-09 20:41:51 +0530

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,21 @@
-import React, { useEffect } from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import { ThemeProvider } from './contexts/ThemeContext.jsx';
-import { AuthProvider } from './contexts/AuthContext.jsx';
-import { testFirebaseConnection } from './firebase.js';
+import React, { useEffect } from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { ThemeProvider } from "./contexts/ThemeContext.jsx";
+import { AuthProvider } from "./contexts/AuthContext.jsx";
+import { testFirebaseConnection } from "./firebase.js";
 
-import Navbar from './components/layout/Navbar.jsx';
-import Footer from './components/layout/Footer.jsx';
-import ParticleBackground from './components/particles/ParticaleBackground.jsx';
-import HomePage from './pages/HomePage.jsx';
-import EventsPage from './pages/EventsPage.jsx';
-import UpcomingActivities from './pages/UpcomingActivities.jsx';
-import JoinClub from './pages/JoinClub.jsx';
-import AdminLogin from './pages/AdminLogin.jsx';
-import CommitteeMembers from './pages/admin/CommitteeMembers.jsx';
-import FormSubmissions from './pages/admin/FormSubmissions.jsx';
-import CommunityMembers from './pages/admin/CommunityMembers.jsx';
-import ProtectedRoute from './components/ProtectedRoute.jsx';
+import Navbar from "./components/layout/Navbar.jsx";
+import Footer from "./components/layout/Footer.jsx";
+import ParticleBackground from "./components/particles/ParticaleBackground.jsx";
+import HomePage from "./pages/HomePage.jsx";
+import EventsPage from "./pages/EventsPage.jsx";
+import UpcomingActivities from "./pages/UpcomingActivities.jsx";
+import JoinClub from "./pages/JoinClub.jsx";
+import AdminLogin from "./pages/AdminLogin.jsx";
+import CommitteeMembers from "./pages/admin/CommitteeMembers.jsx";
+import FormSubmissions from "./pages/admin/FormSubmissions.jsx";
+import CommunityMembers from "./pages/admin/CommunityMembers.jsx";
+import ProtectedRoute from "./components/ProtectedRoute.jsx";
 
 function App() {
   useEffect(() => {
